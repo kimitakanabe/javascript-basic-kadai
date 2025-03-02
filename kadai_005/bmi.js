dbd05d82a6b0d6f6feef3554ffcadfc487491285
Kimi <kimiko.takanabe@gmail.com>
2025-03-02 17:14:24 +0100

    bmi.jsを再アップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 変数の宣言
+let userWeight;
+let userHeight;
+let userBMI;
+
+// 値の代入
+userWeight = 68;
+userHeight = 1.7;
+userBMI = userWeight / (userHeight * userHeight);
+
+// コンソールへの出力
+console.log(userBMI);
